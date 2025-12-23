b423583a67ee08a01dcf56f0a046d93b849d5aab
Azuki.I <azuking69@hotmail.co.jp>
2025-12-23 16:16:45 +0900

    25.12.23 count3

--- Demo.sql
+++ Demo.sql
@@ -1,6 +1,6 @@
-SELECT last_name, salary, salary + 300
-FROM   employees;
-SELECT last_name, salary, 12*salary+100
-FROM   employees;
-SELECT last_name, salary, 12*(salary+100)
-FROM   employees;
+SELECT last_name, job_id, department_id
+FROM   employees
+WHERE  last_name = 'Whalen';
+SELECT last_name, job_id, hire_date
+FROM   employees
+WHERE  hire_date = '14/11/16';
---
+++ 인사관리.sql
@@ -0,0 +1,8 @@
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary >=10000
+AND    job_id LIKE '%MAN%';
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary >= 10000
+OR     job_id LIKE '%MAN%';
